aec1b7b07c15e33c5ad5e352f11a5173c1336dc7
granthughes1999 <grant.2.hughes@cuanschutz.edu>
2022-04-20 00:01:05 -0600

    photo autoplay mobile test

--- apps.js
+++ apps.js
@@ -154,3 +154,11 @@ $(document).ready(function () {
 $(window).scroll(function () {
   showImages('.star');
 });
+
+// image scroll
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
